489ff51396f3d3682100b7f35c9c7fb7c4dbc6aa
tkillip7 <tkp3170@gmail.com>
2023-01-25 17:07:45 -0500

    keycode left

--- tetris/script.js
+++ tetris/script.js
@@ -69,6 +69,17 @@ document.addEventListener('DOMContentLoaded', () => {
       //make the tetromino move down every second
       timerId = setInterval(moveDown, 1000)
 
+      //assign functions to keyCodes
+      function control(e) {
+        if(e.keyCode === 37) {
+            moveLeft()
+        }
+      }
+      document.addEventListener('keyup', control)
+
+
+
+
       //move down function
       function moveDown() {
         undraw()
@@ -91,6 +102,25 @@ document.addEventListener('DOMContentLoaded', () => {
       }
 
 
+      //move the tetromino left, unless is at the edge or there is a blockage
+      function moveLeft() {
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+        if(!isAtLeftEdge) currentPosition -= 1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+            currentPosition += 1
+        }
+
+        draw()
+
+      }
+
+
+
+
+
 
 
 
@@ -99,37 +129,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
  
 
- /*const lTetromino = [
-    [1, width + 1, width * 2 + 1, 2],
-    [width, width + 1, width + 2, width * 2 + 2],
-    [1, width + 1, width * 2 + 1, width * 2],
-    [width, width * 2, width * 2 + 1, width * 2 + 2]
-  ];
-
-  const zTetromino = [
-    [0, width, width + 1, width * 2 + 1],
-    [width + 1, width + 2, width * 2, width * 2 + 1],
-    [0, width, width + 1, width * 2 + 1],
-    [width + 1, width + 2, width * 2, width * 2 + 1]
-  ];
-
-  const tTetromino = [
-    [1, width, width + 1, width + 2],
-    [1, width + 1, width + 2, width * 2 + 1],
-    [width, width + 1, width + 2, width * 2 + 1],
-    [1, width, width + 1, width * 2 + 1]
-  ];
-
-  const oTetromino = [
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1]
-  ];
-
-  const iTetromino = [
-    [1, width + 1, width * 2 + 1, width * 3 + 1],
-    [width, width + 1, width + 2, width + 3],
-    [1, width + 1, width * 2 + 1, width * 3 + 1],
-    [width, width + 1, width + 2, width + 3]
-  ]; */
+ 
